2f10952a331d870e16f7c02dd1372eb6d6b16a55
Vitor Azevedo Padovani <98499539+Vitor-Padovani@users.noreply.github.com>
2024-07-28 22:14:05 -0300

    só aceita

--- src/modules/lexer.py
+++ src/modules/lexer.py
@@ -1,6 +1,6 @@
 class Lexer:
     def __init__(self):
-        self
+        pass
 
     def run(self, string) -> 'list[str]':
         # Método público que chama o método privado __string2tokens__ para analisar a string e retornar uma lista de tokens
@@ -19,9 +19,18 @@ class Lexer:
                 current_position += 1
                 continue
 
-            elif current_char.isdigit():
-                # Reconhece números inteiros
+            # Um número inteiro pode ser positivo ou negativo
+            # Para se reconhecer um número negativo, primeiro verifica-se se o caracter atual é '-'
+            # Se for o caso, temos duas possibilidades de ser um número negativo: ser o começo da expressão ou o token anterior ser '+-*/('
+            # Para o segundo caso, verifica-se se existem tokens, se o último adicionado foi uma string, e se pertence a '+-*/(' respectivamente
+            elif current_char.isdigit() or (current_char == '-' and (
+                    current_position == 0 or
+                    (tokens and isinstance(tokens[-1], str) and tokens[-1] in '+-*/('))):
+                # Reconhece números inteiros e números negativos
                 start_position = current_position
+                if current_char == '-':
+                    current_position += 1
+
                 while current_position < len(string) and string[current_position].isdigit():
                     current_position += 1
 
@@ -45,5 +54,5 @@ class Lexer:
             else:
                 # Levanta uma exceção para caracteres inesperados
                 raise RuntimeError(f'Caractere inesperado: {current_char}')
-        
+
         return tokens
